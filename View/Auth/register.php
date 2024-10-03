9d7216ab7e546d16dd146231bed0b53ce5d7a232
AnasHefied <126481899+AnasHefied@users.noreply.github.com>
2024-10-03 19:12:38 +0200

    register

---
+++ View/Auth/register.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="../css/register.css"> <!-- Lien vers le fichier CSS -->
+</head>
+<body>
+    <div class="register-container">
+        <h2>Inscription</h2>
+        <form method="POST" action="../../Controller/index_app.php?action=register">
+            <input type="text" name="username" placeholder="Nom d'utilisateur" required>
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Mot de passe" required>
+            <select name="role">
+                <option value="admin">Admin</option>
+                <option value="doctor">Médecin</option>
+                <option value="patient">Patient</option>
+            </select>
+
+            <button type="submit">S'inscrire</button>
+        </form>
+
+        <?php if (isset($_GET['error'])): ?>
+            <p class="error-message"><?php echo $_GET['error']; ?></p>
+        <?php endif; ?>
+
+        <p class="login-link">Déjà inscrit? 
+        <a href="../Auth/login.php">Connectez-vous ici.</a>
+        </p>
+    </div>
+</body>
+</html>
