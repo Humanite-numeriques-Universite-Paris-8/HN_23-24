c5f09ab6a7f36b467a402ba24c8edc20def69883
AnasHefied <126481899+AnasHefied@users.noreply.github.com>
2024-10-24 21:47:25 +0200

    Vocabulaire

---
+++ data/bdd.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 24 oct. 2024 à 17:39
+-- Version du serveur : 8.0.39
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `anas_thyp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `resource_template`
+--
+
+DROP TABLE IF EXISTS `resource_template`;
+CREATE TABLE IF NOT EXISTS `resource_template` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `owner_id` int DEFAULT NULL,
+  `resource_class_id` int DEFAULT NULL,
+  `title_property_id` int DEFAULT NULL,
+  `description_property_id` int DEFAULT NULL,
+  `label` varchar(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_39ECD52EEA750E8` (`label`),
+  KEY `IDX_39ECD52E7E3C61F9` (`owner_id`),
+  KEY `IDX_39ECD52E448CC1BD` (`resource_class_id`),
+  KEY `IDX_39ECD52E724734A3` (`title_property_id`),
+  KEY `IDX_39ECD52EB84E0D1D` (`description_property_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `resource_template`
+--
+
+INSERT INTO `resource_template` (`id`, `owner_id`, `resource_class_id`, `title_property_id`, `description_property_id`, `label`) VALUES
+(4, 1, 94, NULL, NULL, 'Anas_etudiant_M2_THYP_Turtle');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `resource_template`
+--
+ALTER TABLE `resource_template`
+  ADD CONSTRAINT `FK_39ECD52E448CC1BD` FOREIGN KEY (`resource_class_id`) REFERENCES `resource_class` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_39ECD52E724734A3` FOREIGN KEY (`title_property_id`) REFERENCES `property` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_39ECD52E7E3C61F9` FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_39ECD52EB84E0D1D` FOREIGN KEY (`description_property_id`) REFERENCES `property` (`id`) ON DELETE SET NULL;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
