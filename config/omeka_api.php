c7431894f6a9640172ec1154bb310b51bd150eb7
AnasHefied <126481899+AnasHefied@users.noreply.github.com>
2024-12-05 22:07:17 +0100

    les files inutile

--- config/omeka_api.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Configuration des clés API pour Omeka S
-define('OMEKA_API_BASE_URL', 'http://localhost/Sites/omeka-s/api/');
-define('OMEKA_API_KEY_IDENTITY', 'wTw5CcChfG4w14oKJKs1wqrkcwN8CUpD');
-define('OMEKA_API_KEY_CREDENTIAL', 'c1VI6sYCtNslYEsa66LVAX1xIpb7kQIr');
-?>
